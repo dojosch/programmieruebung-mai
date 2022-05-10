3ee36f1205b8db789c726ac2b2bb5b447f4960d3
Dominik Schratl <hi@dominik-schratl.at>
2022-05-10 15:03:00 +0200

    Ex 2

---
+++ angabe_methode.cs
@@ -0,0 +1,10 @@
+// YOUR CODE HERE
+
+double verkaufspreis = 299.99;
+
+Console.WriteLine("Gib deinen Gutscheincode ein:");
+string gutscheincode = Console.ReadLine();
+
+// YOUR CODE HERE
+
+Console.WriteLine($"Dein neuer Preis beträgt: {rabattPreis}");
