1e4fb2bf3e860127761e6583987ced76194e8c0b
Dominik Schratl <hi@dominik-schratl.at>
2022-05-10 14:23:33 +0200

    Ex 1

---
+++ angabe_array.cs
@@ -0,0 +1,6 @@
+double[] temperatures = {  15.8, 24.58, 37.19, 22.45, 15.32, 22.24, 18.69, 21.15, 33.88, 36.55, 18.1, 24.68, 25.46, 24.67, 25.59, 33.66, 30.09, 25.77, 21.34, 28.42, 35.59, 21.11, 28.74, 33.71, 16.71, 19.5, 16.28, 18.41, 26.19, 34.47 };
+int tmpOutOfRangeCount = 0;
+
+// YOUR CODE HERE
+
+Console.WriteLine($"Die Temperatur hat den optimalen Bereich {tmpOutOfRangeCount} mal verlassen.");
