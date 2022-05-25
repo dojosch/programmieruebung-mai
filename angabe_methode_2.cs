9f2d5d906f4306f8b0536df7538804699ad69e00
Dominik Schratl <hi@dominik-schratl.at>
2022-05-25 08:58:06 +0200

    Ex 2 . 2

---
+++ angabe_methode_2.cs
@@ -0,0 +1,12 @@
+// YOUR CODE HERE
+
+double materialpreis = 299.99;
+
+Console.WriteLine("Welches Material wird verwendet?");
+Console.WriteLine("1: Material 1");
+Console.WriteLine("2: Material 2");
+int materialId = Convert.ToInt32(Console.ReadLine());
+
+// YOUR CODE HERE
+
+Console.WriteLine($"Dein Gesamtpreis beträgt: {materialpreis}");
